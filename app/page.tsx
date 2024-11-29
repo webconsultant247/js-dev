95b44eb10354309f7d67a0f312cda458f69ac091
Ali Khansari <webconsultant247@gmail.com>
2024-11-29 12:49:52 -0600

    Prepare the assets and metadata and tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl text-violet-700 font-black flex flex-1">
-      Welcome to the world of Next.js
-    </h1>
+    <>
+      <h1 className="font-bold text-dark-500">
+        Welcome to the world of Next.js
+      </h1>
+      <h1 className="font-space-grotesk font-bold text-dark-500">
+        Welcome to the world of Next.js
+      </h1>
+    </>
   );
 }
