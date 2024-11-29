5e0dc183241ebb6bb40df9efa0a82784443f3fc7
Ali Khansari <webconsultant247@gmail.com>
2024-11-29 14:44:19 -0600

    Install Light & Dark Theme, Shadcn and Navbar

--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -1,7 +1,30 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
+import Theme from "./Theme";
+
 const Navbar = () => {
-  return <nav className="flex-between">Navbar</nav>;
+  return (
+    <nav className="flex-between background-light-900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
+      <Link className="flex items-center gap-1" href="/">
+        <Image
+          src="/images/site-logo.svg"
+          width={23}
+          height={23}
+          alt="DevFlow Logo"
+        />
+        <p className="h2-bold font-space-grotesk text-dark-100 dark:text-light-900 max-sm:hidden">
+          Dev
+          <span className="text-primary-500">Flow</span>
+        </p>
+      </Link>
+      <p>Global Search</p>
+      <div className="flex-between gap-5">
+        <Theme />
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
