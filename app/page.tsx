24e806a9cb480fd00ec2f68c8cfad20b62eefc06
Ali Khansari <webconsultant247@gmail.com>
2024-11-29 12:16:12 -0600

    ESlint Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl text-violet-700 font-black">
+    <h1 className="text-3xl text-violet-700 font-black flex flex-1">
       Welcome to the world of Next.js
     </h1>
   );
